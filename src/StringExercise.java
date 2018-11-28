fae817eca2e6796f70ffc1af7c31f9764fb54128
karen-rivas0410 <karen-rivas0410@gmail.com>
2018-11-27 20:04:42 -0600

    added solutions to exercise for strings

---
+++ src/StringExercise.java
@@ -0,0 +1,22 @@
+public class StringExercise {
+    public static void main(String [] arg){
+//        String Basics. Create a class named StringExercise with a main method.
+//        For each of the following output examples, create a string variable named message that contains the desired output
+//        and print it out to the console.
+//        Do this with only one string variable and one print statement.
+//        We don't need no education
+//        We don't need no thought control
+//        Check "this" out!, "s inside of "s!
+//        In windows, the main drive is usually C:\
+//        I can do backslashes \, double backslashes \\,
+//        and the amazing triple backslash \\\!
+
+        String message = "";
+        message += "We don't need no education";
+        message += "\nWe don't need no thought control";
+        message += "\n\nCheck \"this\" out!, \"s inside of \"s!";
+        message += "\n\nIn windows, the main drive is usually C:\\";
+        message += "\n\nI can do the backslashes \\, double backslashes \\\\, \nand the amazing triple backslash \\\\\\!";
+        System.out.println(message);
+    }
+}
